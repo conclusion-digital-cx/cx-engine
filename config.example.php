0dfb076a7a79a7ea5d85d791eed4904463ebeea6
Jelle Hak <jellehak@gmail.com>
2020-02-17 22:18:29 +0100

    cleanup

--- config.example.php
+++ config.example.php
@@ -1,7 +1,24 @@
 <?php
 
 return (object)[
+    'tokenSecret' => 'veryVerySecret',
+    'root' => __DIR__,
+    'baseUrl' => 'http://localhost:8666',   // used for uploads, etc.
+    'apiKeys' => [
+        'yourSecretApiKey'
+    ],
+    'autoload' => [
+        // 'editor',
+        // 'contenttools'
+    ],
     'theme' => '2020',
+    'debug' => true,
     'uploaddir' => __DIR__."/uploads", 
-    'dbpath' => __DIR__."/test.db"  // __DIR__."/storage/test.db"
+    'db' => [
+        // https://medoo.in/
+        'database_type' => 'sqlite',
+        'database_file' => __DIR__."/db.db",
+        'server' => 'localhost',
+        "logging" => true,
+    ]
 ];
